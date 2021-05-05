0cbaf2a9ee290b11bb0100ab4046654a44118bdb
Ahmed-Malabi <Ahmed.N.Malabi@gmail.com>
2021-05-05 14:46:18 -0500

    Added score activity

---
+++ app/src/main/java/com/srisubhashp/teachingtodlersapp/ScoreActivity.java
@@ -0,0 +1,37 @@
+package com.srisubhashp.teachingtodlersapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    private TextView score;
+    private Button cont;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+
+        score = findViewById(R.id.sa_scoreView);
+        cont = findViewById(R.id.sa_continueButton);
+
+        String score_str = getIntent().getStringExtra("SCORE");
+        score.setText(score_str);
+
+
+        cont.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScoreActivity.this,MainActivity.class);
+                ScoreActivity.this.startActivity(intent);
+                ScoreActivity.this.finish();
+            }
+        });
+    }
+}
